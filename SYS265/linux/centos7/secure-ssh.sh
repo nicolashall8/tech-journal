aaff3c56784d12b33180e4a9ffed8c74b78fa84e
nicolashall8 <nicolas.hall@mymail.champlain.edu>
2021-03-11 16:13:10 -0500

    added secure-ssh file

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author nicolashall8
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
