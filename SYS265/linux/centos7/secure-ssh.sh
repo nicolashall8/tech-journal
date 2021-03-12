242468755c5c5ed7c8727453495188665610e1fd
nicolashall8 <nicolas.hall@mymail.champlain.edu>
2021-03-12 00:59:25 +0000

    updated secure-ssh file

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,12 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+#!/bin/bash
+echo Type in a Username:
+read uname
+sudo useradd -m -d /home/$uname -s /bin/bash $uname
+sudo mkdir /home/$uname/.ssh
+sudo cp ~/tech-journal/SYS265/linux/public-keys/id_rsa.pub /home/$uname/.ssh/authorized_keys
+sudo chmod 700 /home/$uname/.ssh
+sudo chmod 600 /home/$uname/.ssh/authorized_keys
+sudo chown -R $uname:$uname /home/$uname/.ssh
