48c4ebb310623aeb924effc82f1a871f40fcfab5
nicolashall8 <71106483+nicolashall8@users.noreply.github.com>
2023-06-03 15:31:55 -0400

    Update create-domain.ps1

--- Windows/PowerShell/create-domain.ps1
+++ Windows/PowerShell/create-domain.ps1
@@ -1,1 +1,15 @@
+# AD DS Deployment
 
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NIDS" `
+-DomainMode "WinThreshold" `
+-DomainName "testnet.local" `
+-DomainNetbiosName "TESTNET" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NIDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
